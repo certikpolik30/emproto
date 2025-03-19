fc933ef4b5c1b5b933e605da56bf25cdcfa3086e
certikpolik30 <evilpostma@gmail.com>
2025-03-19 19:43:10 +0100

    Update transport.py

--- transport.py
+++ transport.py
@@ -1,15 +1,15 @@
 import socket
 import struct
 
-# === Odesílání a přijímání zpráv ===
-def send_encrypted_message(sock, encrypted_message):
+# === Odesílání a přijímání zpráv přes TCP ===
+def send_encrypted_message_tcp(sock, encrypted_message):
     """Odešle šifrovanou zprávu přes TCP socket"""
     obfuscated_message = obfuscate_data(encrypted_message)
     message_length = len(obfuscated_message)
     sock.sendall(struct.pack("!I", message_length))
     sock.sendall(obfuscated_message)
 
-def receive_encrypted_message(sock):
+def receive_encrypted_message_tcp(sock):
     """Přijme šifrovanou zprávu přes TCP socket"""
     message_length_data = sock.recv(4)
     if not message_length_data:
@@ -18,8 +18,24 @@ def receive_encrypted_message(sock):
     obfuscated_message = sock.recv(message_length)
     return deobfuscate_data(obfuscated_message)
 
-# === Odesílání a přijímání souborů ===
-def send_encrypted_file(sock, encrypted_file_data, file_name):
+# === Odesílání a přijímání zpráv přes UDP ===
+def send_encrypted_message_udp(sock, encrypted_message, address):
+    """Odešle šifrovanou zprávu přes UDP socket"""
+    obfuscated_message = obfuscate_data(encrypted_message)
+    message_length = len(obfuscated_message)
+    sock.sendto(struct.pack("!I", message_length) + obfuscated_message, address)
+
+def receive_encrypted_message_udp(sock):
+    """Přijme šifrovanou zprávu přes UDP socket"""
+    message_length_data, _ = sock.recvfrom(4)
+    if not message_length_data:
+        return None
+    message_length = struct.unpack("!I", message_length_data)[0]
+    obfuscated_message, _ = sock.recvfrom(message_length)
+    return deobfuscate_data(obfuscated_message)
+
+# === Odesílání a přijímání souborů přes TCP ===
+def send_encrypted_file_tcp(sock, encrypted_file_data, file_name):
     """Odešle šifrovaný soubor přes TCP socket"""
     obfuscated_file_data = obfuscate_data(encrypted_file_data)
     file_name_encoded = file_name.encode()
@@ -31,7 +47,7 @@ def send_encrypted_file(sock, encrypted_file_data, file_name):
     sock.sendall(struct.pack("!Q", file_data_length))
     sock.sendall(obfuscated_file_data)
 
-def receive_encrypted_file(sock):
+def receive_encrypted_file_tcp(sock):
     """Přijme šifrovaný soubor přes TCP socket"""
     file_name_length_data = sock.recv(4)
     if not file_name_length_data:
@@ -44,3 +60,27 @@ def receive_encrypted_file(sock):
     obfuscated_file_data = sock.recv(file_data_length)
 
     return file_name, deobfuscate_data(obfuscated_file_data)
+
+# === Odesílání a přijímání souborů přes UDP ===
+def send_encrypted_file_udp(sock, encrypted_file_data, file_name, address):
+    """Odešle šifrovaný soubor přes UDP socket"""
+    obfuscated_file_data = obfuscate_data(encrypted_file_data)
+    file_name_encoded = file_name.encode()
+    file_name_length = len(file_name_encoded)
+    file_data_length = len(obfuscated_file_data)
+
+    sock.sendto(struct.pack("!I", file_name_length) + file_name_encoded + struct.pack("!Q", file_data_length) + obfuscated_file_data, address)
+
+def receive_encrypted_file_udp(sock):
+    """Přijme šifrovaný soubor přes UDP socket"""
+    file_name_length_data, _ = sock.recvfrom(4)
+    if not file_name_length_data:
+        return None, None
+    file_name_length = struct.unpack("!I", file_name_length_data)[0]
+    file_name, _ = sock.recvfrom(file_name_length)
+
+    file_data_length_data, _ = sock.recvfrom(8)
+    file_data_length = struct.unpack("!Q", file_data_length_data)[0]
+    obfuscated_file_data, _ = sock.recvfrom(file_data_length)
+
+    return file_name.decode(), deobfuscate_data(obfuscated_file_data)
