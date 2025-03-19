78a61e0257915913d78b95af8a223828061d85f4
certikpolik30 <evilpostma@gmail.com>
2025-03-19 21:45:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vmproto',
-    version='0.3.0',
+    version='0.4.0',
     packages=find_packages(),
     description='A secure communication protocol for e2ee.',
     author='Alfisene Keita',
