7a6560d2db5e2862a7d2d2e22bce71375c01d135
certikpolik30 <evilpostma@gmail.com>
2025-03-19 17:51:11 +0100

    Update transport.py

--- transport.py
+++ transport.py
@@ -1,49 +1,27 @@
 import socket
 import struct
-import threading
-import os
-from .encryption import (
-    emproto_encrypt,
-    emproto_decrypt,
-    derive_shared_key,
-    generate_x3dh_bundle
-)
+from encryption import encrypt, decrypt
 
-# Funkce pro příjem binárních dat přes socket
-def recv_packet(sock: socket.socket):
-    length_data = sock.recv(4)
-    if not length_data:
+def send_encrypted(sock, auth_key, payload, session_id, sequence_number):
+    """
+    Odešle šifrovaná data přes socket.
+    """
+    encrypted_packet = encrypt(auth_key, payload, session_id, sequence_number)
+    packet_len = struct.pack('>I', len(encrypted_packet))
+    sock.sendall(packet_len + encrypted_packet)
+
+def recv_encrypted(sock, auth_key):
+    """
+    Přijme a dešifruje data ze socketu.
+    """
+    packet_len_bytes = sock.recv(4)
+    if not packet_len_bytes:
         return None
-    length = struct.unpack('>I', length_data)[0]
-    data = b""
-    while len(data) < length:
-        chunk = sock.recv(length - len(data))
-        if not chunk:
+    packet_len = struct.unpack('>I', packet_len_bytes)[0]
+    packet = b''
+    while len(packet) < packet_len:
+        data = sock.recv(packet_len - len(packet))
+        if not data:
             break
-        data += chunk
-    return data
-
-# Funkce pro odeslání binárních dat přes socket
-def send_packet(sock: socket.socket, data: bytes):
-    sock.sendall(struct.pack('>I', len(data)) + data)
-
-# Funkce pro inicializaci secure kanálu (exchange X3DH public key)
-def exchange_keys(sock: socket.socket, own_bundle, store_shared_key_callback):
-    # pošli public key
-    send_packet(sock, own_bundle['identity_pub'])
-    # přijmi peer public key
-    peer_key = recv_packet(sock)
-    shared_key = derive_shared_key(own_bundle['identity_key'], peer_key)
-    store_shared_key_callback(shared_key)
-
-# Funkce pro šifrované odesílání zpráv nebo souborů
-def secure_send(sock: socket.socket, auth_key: bytes, session_id: int, seq_no: int, data: bytes):
-    encrypted = emproto_encrypt(auth_key, data, session_id, seq_no)
-    send_packet(sock, encrypted)
-
-# Funkce pro příjem šifrovaných dat
-def secure_recv(sock: socket.socket, auth_key: bytes, session_id: int):
-    encrypted_packet = recv_packet(sock)
-    if encrypted_packet is None:
-        return None
-    return emproto_decrypt(auth_key, encrypted_packet, session_id)
+        packet += data
+    return decrypt(auth_key, packet)
