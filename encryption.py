bde539e13cb805306c097246873b85b95e3a62fe
certikpolik30 <evilpostma@gmail.com>
2025-03-19 16:25:29 +0100

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,1 +1,57 @@
-IMPLEMENT ENCRYPTION PROTOCOL CODE HERE
+import os
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+class EncryptionLayer:
+    def __init__(self):
+        self.private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=3072
+        )
+        self.public_key = self.private_key.public_key()
+
+    def generate_rsa_keys(self):
+        private_key = rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=3072
+        )
+        public_key = private_key.public_key()
+        return private_key, public_key
+
+    def encrypt_message(self, message, public_key):
+        encrypted_message = public_key.encrypt(
+            message,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return encrypted_message
+
+    def decrypt_message(self, encrypted_message, private_key):
+        decrypted_message = private_key.decrypt(
+            encrypted_message,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        return decrypted_message
+
+    def encrypt_file(self, file_path, key):
+        with open(file_path, 'rb') as file:
+            data = file.read()
+        aesgcm = AESGCM(key)
+        nonce = os.urandom(12)
+        encrypted_data = aesgcm.encrypt(nonce, data, None)
+        return nonce + encrypted_data
+
+    def decrypt_file(self, encrypted_data, key):
+        nonce = encrypted_data[:12]
+        ciphertext = encrypted_data[12:]
+        aesgcm = AESGCM(key)
+        decrypted_data = aesgcm.decrypt(nonce, ciphertext, None)
+        return decrypted_data
