dcbbabe7a53a9103fe8f3be3f783535337a8f361
certikpolik30 <evilpostma@gmail.com>
2025-03-19 19:00:54 +0100

    Update transport.py

--- transport.py
+++ transport.py
@@ -4,9 +4,10 @@ import struct
 # === Odesílání a přijímání zpráv ===
 def send_encrypted_message(sock, encrypted_message):
     """Odešle šifrovanou zprávu přes TCP socket"""
-    message_length = len(encrypted_message)
+    obfuscated_message = obfuscate_data(encrypted_message)
+    message_length = len(obfuscated_message)
     sock.sendall(struct.pack("!I", message_length))
-    sock.sendall(encrypted_message)
+    sock.sendall(obfuscated_message)
 
 def receive_encrypted_message(sock):
     """Přijme šifrovanou zprávu přes TCP socket"""
@@ -14,19 +15,21 @@ def receive_encrypted_message(sock):
     if not message_length_data:
         return None
     message_length = struct.unpack("!I", message_length_data)[0]
-    return sock.recv(message_length)
+    obfuscated_message = sock.recv(message_length)
+    return deobfuscate_data(obfuscated_message)
 
 # === Odesílání a přijímání souborů ===
 def send_encrypted_file(sock, encrypted_file_data, file_name):
     """Odešle šifrovaný soubor přes TCP socket"""
+    obfuscated_file_data = obfuscate_data(encrypted_file_data)
     file_name_encoded = file_name.encode()
     file_name_length = len(file_name_encoded)
-    file_data_length = len(encrypted_file_data)
+    file_data_length = len(obfuscated_file_data)
 
     sock.sendall(struct.pack("!I", file_name_length))
     sock.sendall(file_name_encoded)
     sock.sendall(struct.pack("!Q", file_data_length))
-    sock.sendall(encrypted_file_data)
+    sock.sendall(obfuscated_file_data)
 
 def receive_encrypted_file(sock):
     """Přijme šifrovaný soubor přes TCP socket"""
@@ -38,6 +41,6 @@ def receive_encrypted_file(sock):
 
     file_data_length_data = sock.recv(8)
     file_data_length = struct.unpack("!Q", file_data_length_data)[0]
-    encrypted_file_data = sock.recv(file_data_length)
+    obfuscated_file_data = sock.recv(file_data_length)
 
-    return file_name, encrypted_file_data
+    return file_name, deobfuscate_data(obfuscated_file_data)
