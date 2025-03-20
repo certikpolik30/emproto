dfdaf7ef44dde5e4cf0eae04acbb1fd1f24b71b9
certikpolik30 <evilpostma@gmail.com>
2025-03-20 13:34:14 +0100

    Update transport.py

--- transport.py
+++ transport.py
@@ -1,23 +1,25 @@
+import socket
 import struct
-import asyncio  # Added for asynchronous operations
-import zlib  # Added for compression
-from .encryption import Obfuscation, CCAProtection  # Add CCAProtection import
+import asyncio
+import zlib
+from .encryption import Obfuscation, CCAProtection, KeyStore, MessageEncryption, FileEncryption
 
 class TCPTransport:
-    @staticmethod
-    async def send_encrypted_message(sock, encrypted_message, derived_key):
+    def __init__(self, auth_key):
+        self.message_encryption = MessageEncryption(auth_key)
+        self.file_encryption = FileEncryption(auth_key)
+
+    async def send_encrypted_message(self, sock, message):
         """Sends an encrypted message over a TCP socket"""
-        if encrypted_message is None:
-            raise ValueError("Encrypted message cannot be None")
-        cca_tag = CCAProtection.protect_against_cca(derived_key, encrypted_message)
+        encrypted_message = self.message_encryption.encrypt(message)
+        cca_tag = CCAProtection.protect_against_cca(self.message_encryption.keystore.get_latest_key(), encrypted_message)
         obfuscated_message = Obfuscation.obfuscate_data(encrypted_message + cca_tag)
-        compressed_message = zlib.compress(obfuscated_message)  # Compress data
+        compressed_message = zlib.compress(obfuscated_message)
         message_length = len(compressed_message)
         await sock.sendall(struct.pack("!I", message_length))
         await sock.sendall(compressed_message)
 
-    @staticmethod
-    async def receive_encrypted_message(sock, derived_key):
+    async def receive_encrypted_message(self, sock):
         """Receives an encrypted message over a TCP socket"""
         message_length_data = await sock.recv(4)
         if not message_length_data:
@@ -26,22 +28,20 @@ class TCPTransport:
         compressed_message = await sock.recv(message_length)
         if not compressed_message:
             return None
-        obfuscated_message = zlib.decompress(compressed_message)  # Decompress data
+        obfuscated_message = zlib.decompress(compressed_message)
         decrypted_message_with_tag = Obfuscation.deobfuscate_data(obfuscated_message)
-        decrypted_message = decrypted_message_with_tag[:-64]  # remove CCA tag
-        expected_cca_tag = decrypted_message_with_tag[-64:]  # Updated for SHA3-512
-        if not CCAProtection.verify_cca_protection(derived_key, decrypted_message, expected_cca_tag):
+        decrypted_message = decrypted_message_with_tag[:-64]
+        expected_cca_tag = decrypted_message_with_tag[-64:]
+        if not CCAProtection.verify_cca_protection(self.message_encryption.keystore.get_latest_key(), decrypted_message, expected_cca_tag):
             raise ValueError("CCA protection verification failed")
-        return decrypted_message
+        return self.message_encryption.decrypt(decrypted_message)
 
-    @staticmethod
-    async def send_encrypted_file(sock, encrypted_file_data, file_name, derived_key):
+    async def send_encrypted_file(self, sock, file_path, file_name):
         """Sends an encrypted file over a TCP socket"""
-        if encrypted_file_data is None:
-            raise ValueError("Encrypted file data cannot be None")
-        cca_tag = CCAProtection.protect_against_cca(derived_key, encrypted_file_data)
+        encrypted_file_data = self.file_encryption.encrypt(file_path)
+        cca_tag = CCAProtection.protect_against_cca(self.file_encryption.keystore.get_latest_key(), encrypted_file_data)
         obfuscated_file_data = Obfuscation.obfuscate_data(encrypted_file_data + cca_tag)
-        compressed_file_data = zlib.compress(obfuscated_file_data)  # Compress data
+        compressed_file_data = zlib.compress(obfuscated_file_data)
         file_name_encoded = file_name.encode()
         file_name_length = len(file_name_encoded)
         file_data_length = len(compressed_file_data)
@@ -51,8 +51,7 @@ class TCPTransport:
         await sock.sendall(struct.pack("!Q", file_data_length))
         await sock.sendall(compressed_file_data)
 
-    @staticmethod
-    async def receive_encrypted_file(sock, derived_key):
+    async def receive_encrypted_file(self, sock, output_path):
         """Receives an encrypted file over a TCP socket"""
         file_name_length_data = await sock.recv(4)
         if not file_name_length_data:
@@ -66,28 +65,30 @@ class TCPTransport:
         if not compressed_file_data:
             return None, None
 
-        obfuscated_file_data = zlib.decompress(compressed_file_data)  # Decompress data
+        obfuscated_file_data = zlib.decompress(compressed_file_data)
         decrypted_file_data_with_tag = Obfuscation.deobfuscate_data(obfuscated_file_data)
-        decrypted_file_data = decrypted_file_data_with_tag[:-64]  # remove CCA tag
-        expected_cca_tag = decrypted_file_data_with_tag[-64:]  # Updated for SHA3-512
-        if not CCAProtection.verify_cca_protection(derived_key, decrypted_file_data, expected_cca_tag):
+        decrypted_file_data = decrypted_file_data_with_tag[:-64]
+        expected_cca_tag = decrypted_file_data_with_tag[-64:]
+        if not CCAProtection.verify_cca_protection(self.file_encryption.keystore.get_latest_key(), decrypted_file_data, expected_cca_tag):
             raise ValueError("CCA protection verification failed")
+        self.file_encryption.decrypt(decrypted_file_data, output_path)
         return file_name, decrypted_file_data
 
 class UDPTransport:
-    @staticmethod
-    async def send_encrypted_message(sock, encrypted_message, address, derived_key):
+    def __init__(self, auth_key):
+        self.message_encryption = MessageEncryption(auth_key)
+        self.file_encryption = FileEncryption(auth_key)
+
+    async def send_encrypted_message(self, sock, message, address):
         """Sends an encrypted message over a UDP socket"""
-        if encrypted_message is None:
-            raise ValueError("Encrypted message cannot be None")
-        cca_tag = CCAProtection.protect_against_cca(derived_key, encrypted_message)
+        encrypted_message = self.message_encryption.encrypt(message)
+        cca_tag = CCAProtection.protect_against_cca(self.message_encryption.keystore.get_latest_key(), encrypted_message)
         obfuscated_message = Obfuscation.obfuscate_data(encrypted_message + cca_tag)
-        compressed_message = zlib.compress(obfuscated_message)  # Compress data
+        compressed_message = zlib.compress(obfuscated_message)
         message_length = len(compressed_message)
         await sock.sendto(struct.pack("!I", message_length) + compressed_message, address)
 
-    @staticmethod
-    async def receive_encrypted_message(sock, derived_key):
+    async def receive_encrypted_message(self, sock):
         """Receives an encrypted message over a UDP socket"""
         message_length_data, _ = await sock.recvfrom(4)
         if not message_length_data:
@@ -96,30 +97,27 @@ class UDPTransport:
         compressed_message, _ = await sock.recvfrom(message_length)
         if not compressed_message:
             return None
-        obfuscated_message = zlib.decompress(compressed_message)  # Decompress data
+        obfuscated_message = zlib.decompress(compressed_message)
         decrypted_message_with_tag = Obfuscation.deobfuscate_data(obfuscated_message)
-        decrypted_message = decrypted_message_with_tag[:-64]  # remove CCA tag
-        expected_cca_tag = decrypted_message_with_tag[-64:]  # Updated for SHA3-512
-        if not CCAProtection.verify_cca_protection(derived_key, decrypted_message, expected_cca_tag):
+        decrypted_message = decrypted_message_with_tag[:-64]
+        expected_cca_tag = decrypted_message_with_tag[-64:]
+        if not CCAProtection.verify_cca_protection(self.message_encryption.keystore.get_latest_key(), decrypted_message, expected_cca_tag):
             raise ValueError("CCA protection verification failed")
-        return decrypted_message
+        return self.message_encryption.decrypt(decrypted_message)
 
-    @staticmethod
-    async def send_encrypted_file(sock, encrypted_file_data, file_name, address, derived_key):
+    async def send_encrypted_file(self, sock, file_path, file_name, address):
         """Sends an encrypted file over a UDP socket"""
-        if encrypted_file_data is None:
-            raise ValueError("Encrypted file data cannot be None")
-        cca_tag = CCAProtection.protect_against_cca(derived_key, encrypted_file_data)
+        encrypted_file_data = self.file_encryption.encrypt(file_path)
+        cca_tag = CCAProtection.protect_against_cca(self.file_encryption.keystore.get_latest_key(), encrypted_file_data)
         obfuscated_file_data = Obfuscation.obfuscate_data(encrypted_file_data + cca_tag)
-        compressed_file_data = zlib.compress(obfuscated_file_data)  # Compress data
+        compressed_file_data = zlib.compress(obfuscated_file_data)
         file_name_encoded = file_name.encode()
         file_name_length = len(file_name_encoded)
         file_data_length = len(compressed_file_data)
 
         await sock.sendto(struct.pack("!I", file_name_length) + file_name_encoded + struct.pack("!Q", file_data_length) + compressed_file_data, address)
 
-    @staticmethod
-    async def receive_encrypted_file(sock, derived_key):
+    async def receive_encrypted_file(self, sock, output_path):
         """Receives an encrypted file over a UDP socket"""
         file_name_length_data, _ = await sock.recvfrom(4)
         if not file_name_length_data:
@@ -133,10 +131,5 @@ class UDPTransport:
         if not compressed_file_data:
             return None, None
 
-        obfuscated_file_data = zlib.decompress(compressed_file_data)  # Decompress data
-        decrypted_file_data_with_tag = Obfuscation.deobfuscate_data(obfuscated_file_data)
-        decrypted_file_data = decrypted_file_data_with_tag[:-64]  # remove CCA tag
-        expected_cca_tag = decrypted_file_data_with_tag[-64:]  # Updated for SHA3-512
-        if not CCAProtection.verify_cca_protection(derived_key, decrypted_file_data, expected_cca_tag):
-            raise ValueError("CCA protection verification failed")
-        return file_name.decode(), decrypted_file_data
+        obfuscated_file_data = zlib.decompress(compressed_file_data)
+        decrypted_file_data_with_tag = Obfuscation.deobfuscate
