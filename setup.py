ced08527aff1632b6391e359ecde688bd933f7a6
certikpolik30 <evilpostma@gmail.com>
2025-03-20 17:34:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cryptogamax",
-    version="0.1.0",
+    version="0.2.0",
     description="A Super Strong Cipher for secure encryption and decryption.",
     author="Alfisene Keita",
     packages=find_packages(),
