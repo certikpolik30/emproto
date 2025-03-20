cea51ed3cff483eae39cec4fe2b0536aa0e87730
certikpolik30 <evilpostma@gmail.com>
2025-03-20 14:13:35 +0100

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -9,61 +9,6 @@ from cryptography.hazmat.primitives.asymmetric import rsa, ec
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-import qrcode  # Added for QR code generation
-
-class KeyStore:
-    """Class to manage key storage and rotation"""
-    def __init__(self, key_file='keystore.bin', password=None):
-        self.key_file = key_file
-        self.password = password or os.urandom(16)
-        self.keys = self.load_keys()
-
-    def load_keys(self):
-        if os.path.exists(self.key_file):
-            with open(self.key_file, 'rb') as f:
-                encrypted_keys = f.read()
-            salt = encrypted_keys[:16]
-            encrypted_keys = encrypted_keys[16:]
-            kdf = PBKDF2HMAC(
-                algorithm=hashes.SHA256(),
-                length=32,
-                salt=salt,
-                iterations=100000,
-                backend=default_backend()
-            )
-            key = kdf.derive(self.password)
-            cipher = Cipher(algorithms.AES(key), modes.GCM(salt), backend=default_backend())
-            decryptor = cipher.decryptor()
-            keys = decryptor.update(encrypted_keys) + decryptor.finalize()
-            return struct.unpack('Q'*int(len(keys)/8), keys)
-        else:
-            return []
-
-    def save_keys(self):
-        salt = os.urandom(16)
-        kdf = PBKDF2HMAC(
-            algorithm=hashes.SHA256(),
-            length=32,
-            salt=salt,
-            iterations=100000,
-            backend=default_backend()
-        )
-        key = kdf.derive(self.password)
-        cipher = Cipher(algorithms.AES(key), modes.GCM(salt), backend=default_backend())
-        encryptor = cipher.encryptor()
-        encrypted_keys = encryptor.update(struct.pack('Q'*len(self.keys), *self.keys)) + encryptor.finalize()
-        with open(self.key_file, 'wb') as f:
-            f.write(salt + encrypted_keys)
-
-    def rotate_keys(self, new_key):
-        self.keys.append(new_key)
-        if len(self.keys) > 10:  # keep only the latest 10 keys
-            self.keys.pop(0)
-        self.save_keys()
-
-    def get_latest_key(self):
-        return self.keys[-1] if self.keys else None
 
 class ECDH:
     @staticmethod
@@ -86,16 +31,24 @@ class ECDH:
         ).derive(shared_secret)
         return derived_key
 
+class AESGCM:
     @staticmethod
-    def generate_security_code(public_key1, public_key2):
-        """Generates a security code from two public keys"""
-        combined_keys = public_key1.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo) + \
-                        public_key2.public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo)
-        hash_value = hashlib.sha3_512(combined_keys).hexdigest()
-        numeric_code = ''.join(str(int(hash_value[i:i+2], 16)) for i in range(0, len(hash_value), 2))
-        qr = qrcode.make(numeric_code)
-        qr.save("security_code.png")
-        return numeric_code
+    def encrypt(key, plaintext, associated_data=b''):
+        """Encrypts plaintext using AES-256-GCM"""
+        iv = os.urandom(12)  # GCM nonce
+        cipher = Cipher(algorithms.AES(key), modes.GCM(iv), backend=default_backend())
+        encryptor = cipher.encryptor()
+        encryptor.authenticate_additional_data(associated_data)
+        ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+        return iv, ciphertext, encryptor.tag
+
+    @staticmethod
+    def decrypt(key, iv, ciphertext, tag, associated_data=b''):
+        """Decrypts ciphertext using AES-256-GCM"""
+        cipher = Cipher(algorithms.AES(key), modes.GCM(iv, tag), backend=default_backend())
+        decryptor = cipher.decryptor()
+        decryptor.authenticate_additional_data(associated_data)
+        return decryptor.update(ciphertext) + decryptor.finalize()
 
 class AESCTR:
     @staticmethod
@@ -147,11 +100,8 @@ class RSA:
         )
 
 class MessageEncryption:
-    def __init__(self, auth_key):
-        self.auth_key = auth_key
-        self.keystore = KeyStore()
-
-    def encrypt(self, message):
+    @staticmethod
+    def encrypt(auth_key, message):
         """Encrypts a text message (AES-256-CTR)"""
         salt = os.urandom(8)
         session_id = os.urandom(8)
@@ -160,21 +110,19 @@ class MessageEncryption:
         payload = salt + session_id + seq_number + timestamp + message.encode()
 
         msg_key = hashlib.sha3_512(payload).digest()[:32]  # Changed to SHA3-512
-        derived_key = hashlib.sha3_512(self.auth_key + msg_key).digest()  # Changed to SHA3-512
+        derived_key = hashlib.sha3_512(auth_key + msg_key).digest()  # Changed to SHA3-512
         iv, ciphertext = AESCTR.encrypt(derived_key, payload)
-
-        # Rotate keys and save the new key
-        self.keystore.rotate_keys(derived_key)
 
         return msg_key + iv + ciphertext
 
-    def decrypt(self, encrypted_message):
+    @staticmethod
+    def decrypt(auth_key, encrypted_message):
         """Decrypts a text message (AES-256-CTR)"""
         msg_key = encrypted_message[:32]  # Updated for SHA3-512
         iv = encrypted_message[32:48]
         ciphertext = encrypted_message[48:]
 
-        derived_key = hashlib.sha3_512(self.auth_key + msg_key).digest()  # Changed to SHA3-512
+        derived_key = hashlib.sha3_512(auth_key + msg_key).digest()  # Changed to SHA3-512
         decrypted_payload = AESCTR.decrypt(derived_key, iv, ciphertext)
 
         salt = decrypted_payload[:8]
@@ -186,12 +134,9 @@ class MessageEncryption:
         return message
 
 class FileEncryption:
-    def __init__(self, auth_key):
-        self.auth_key = auth_key
-        self.keystore = KeyStore()
-
-    def encrypt(self, file_path):
-        """Encrypts a file using AES-256-CTR"""
+    @staticmethod
+    def encrypt(auth_key, file_path):
+        """Encrypts a file using AES-256-GCM"""
         with open(file_path, 'rb') as f:
             file_data = f.read()
 
@@ -202,22 +147,21 @@ class FileEncryption:
         payload = salt + session_id + seq_number + timestamp + file_data
 
         msg_key = hashlib.sha3_512(payload).digest()[:32]  # Changed to SHA3-512
-        derived_key = hashlib.sha3_512(self.auth_key + msg_key).digest()  # Changed to SHA3-512
-        iv, ciphertext = AESCTR.encrypt(derived_key, payload)
+        derived_key = hashlib.sha3_512(auth_key + msg_key).digest()  # Changed to SHA3-512
+        iv, ciphertext, tag = AESGCM.encrypt(derived_key, payload)
 
-        # Rotate keys and save the new key
-        self.keystore.rotate_keys(derived_key)
+        return msg_key + iv + tag + ciphertext
 
-        return msg_key + iv + ciphertext
-
-    def decrypt(self, encrypted_data, output_path):
-        """Decrypts a file using AES-256-CTR"""
+    @staticmethod
+    def decrypt(auth_key, encrypted_data, output_path):
+        """Decrypts a file using AES-256-GCM"""
         msg_key = encrypted_data[:32]  # Updated for SHA3-512
-        iv = encrypted_data[32:48]
-        ciphertext = encrypted_data[48:]
+        iv = encrypted_data[32:44]
+        tag = encrypted_data[44:60]
+        ciphertext = encrypted_data[60:]
 
-        derived_key = hashlib.sha3_512(self.auth_key + msg_key).digest()  # Changed to SHA3-512
-        decrypted_payload = AESCTR.decrypt(derived_key, iv, ciphertext)
+        derived_key = hashlib.sha3_512(auth_key + msg_key).digest()  # Changed to SHA3-512
+        decrypted_payload = AESGCM.decrypt(derived_key, iv, ciphertext, tag)
 
         with open(output_path, 'wb') as f:
             f.write(decrypted_payload[32:])  # Remove Salt, Session_ID, sequence number, and timestamp
@@ -228,3 +172,5 @@ class SecurityUtils:
         """Verifies message integrity after decryption"""
         calculated_msg_key = hashlib.sha3_512(auth_key + decrypted_message.encode()).digest()[:32]  # Changed to SHA3-512
         return hmac.compare_digest(calculated_msg_key, expected_msg_key)
+
+# Removed CCA protection mechanism
