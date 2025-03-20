8d513731fc71be62a5f723bbf5873f7a4b126186
certikpolik30 <evilpostma@gmail.com>
2025-03-20 10:11:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mvproto',
-    version='0.4',
+    version='0.5',
     packages=find_packages(),
     description='A secure communication protocol for e2ee.',
     author='Alfisene Keita',
