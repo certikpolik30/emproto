179dda5ce475be4e4bc797d9a91fa239a4e4bb9c
certikpolik30 <evilpostma@gmail.com>
2025-03-20 14:51:57 +0100

    Update transport.py

--- transport.py
+++ transport.py
@@ -1,8 +1,11 @@
 import socket
 import struct
-import asyncio  # Added for asynchronous operations
-import zlib  # Added for compression
-from .encryption import ECDH, Obfuscation  # Removed CCAProtection import
+import asyncio
+import zlib
+from .encryption import ECDH  # Removed Obfuscation import
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+
 
 class TCPTransport:
     @staticmethod
@@ -10,8 +13,7 @@ class TCPTransport:
         """Sends an encrypted message over a TCP socket"""
         if encrypted_message is None:
             raise ValueError("Encrypted message cannot be None")
-        obfuscated_message = Obfuscation.obfuscate_data(encrypted_message)
-        compressed_message = zlib.compress(obfuscated_message)  # Compress data
+        compressed_message = zlib.compress(encrypted_message)
         message_length = len(compressed_message)
         await sock.sendall(struct.pack("!I", message_length))
         await sock.sendall(compressed_message)
@@ -26,8 +28,7 @@ class TCPTransport:
         compressed_message = await sock.recv(message_length)
         if not compressed_message:
             return None
-        obfuscated_message = zlib.decompress(compressed_message)  # Decompress data
-        decrypted_message = Obfuscation.deobfuscate_data(obfuscated_message)
+        decrypted_message = zlib.decompress(compressed_message)
         return decrypted_message
 
     @staticmethod
@@ -54,8 +55,7 @@ class TCPTransport:
         """Sends an encrypted file over a TCP socket"""
         if encrypted_file_data is None:
             raise ValueError("Encrypted file data cannot be None")
-        obfuscated_file_data = Obfuscation.obfuscate_data(encrypted_file_data)
-        compressed_file_data = zlib.compress(obfuscated_file_data)  # Compress data
+        compressed_file_data = zlib.compress(encrypted_file_data)
         file_name_encoded = file_name.encode()
         file_name_length = len(file_name_encoded)
         file_data_length = len(compressed_file_data)
@@ -72,7 +72,7 @@ class TCPTransport:
         if not file_name_length_data:
             return None, None
         file_name_length = struct.unpack("!I", file_name_length_data)[0]
-        file_name = await sock.recv(file_name_length).decode()
+        file_name = (await sock.recv(file_name_length)).decode()
 
         file_data_length_data = await sock.recv(8)
         file_data_length = struct.unpack("!Q", file_data_length_data)[0]
@@ -80,9 +80,9 @@ class TCPTransport:
         if not compressed_file_data:
             return None, None
 
-        obfuscated_file_data = zlib.decompress(compressed_file_data)  # Decompress data
-        decrypted_file_data = Obfuscation.deobfuscate_data(obfuscated_file_data)
+        decrypted_file_data = zlib.decompress(compressed_file_data)
         return file_name, decrypted_file_data
+
 
 class UDPTransport:
     @staticmethod
@@ -90,8 +90,7 @@ class UDPTransport:
         """Sends an encrypted message over a UDP socket"""
         if encrypted_message is None:
             raise ValueError("Encrypted message cannot be None")
-        obfuscated_message = Obfuscation.obfuscate_data(encrypted_message)
-        compressed_message = zlib.compress(obfuscated_message)  # Compress data
+        compressed_message = zlib.compress(encrypted_message)
         message_length = len(compressed_message)
         await sock.sendto(struct.pack("!I", message_length) + compressed_message, address)
 
@@ -105,8 +104,7 @@ class UDPTransport:
         compressed_message, _ = await sock.recvfrom(message_length)
         if not compressed_message:
             return None
-        obfuscated_message = zlib.decompress(compressed_message)  # Decompress data
-        decrypted_message = Obfuscation.deobfuscate_data(obfuscated_message)
+        decrypted_message = zlib.decompress(compressed_message)
         return decrypted_message
 
     @staticmethod
@@ -114,13 +112,15 @@ class UDPTransport:
         """Sends an encrypted file over a UDP socket"""
         if encrypted_file_data is None:
             raise ValueError("Encrypted file data cannot be None")
-        obfuscated_file_data = Obfuscation.obfuscate_data(encrypted_file_data)
-        compressed_file_data = zlib.compress(obfuscated_file_data)  # Compress data
+        compressed_file_data = zlib.compress(encrypted_file_data)
         file_name_encoded = file_name.encode()
         file_name_length = len(file_name_encoded)
         file_data_length = len(compressed_file_data)
 
-        await sock.sendto(struct.pack("!I", file_name_length) + file_name_encoded + struct.pack("!Q", file_data_length) + compressed_file_data, address)
+        await sock.sendto(
+            struct.pack("!I", file_name_length) + file_name_encoded + struct.pack("!Q", file_data_length) + compressed_file_data,
+            address
+        )
 
     @staticmethod
     async def receive_encrypted_file(sock, derived_key):
@@ -137,6 +137,5 @@ class UDPTransport:
         if not compressed_file_data:
             return None, None
 
-        obfuscated_file_data = zlib.decompress(compressed_file_data)  # Decompress data
-        decrypted_file_data = Obfuscation.deobfuscate_data(obfuscated_file_data)
+        decrypted_file_data = zlib.decompress(compressed_file_data)
         return file_name.decode(), decrypted_file_data
