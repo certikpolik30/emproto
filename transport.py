0052e176fcf01fb37f93c26c5fa5389cb0cdc871
certikpolik30 <evilpostma@gmail.com>
2025-03-20 12:26:37 +0100

    Update transport.py

--- transport.py
+++ transport.py
@@ -1,21 +1,21 @@
 import socket
 import struct
-import requests
-from .encryption import Obfuscation, X3DH
+from .encryption import Obfuscation, CCAProtection  # Add CCAProtection import
 
 class TCPTransport:
     @staticmethod
-    def send_encrypted_message(sock, encrypted_message):
+    def send_encrypted_message(sock, encrypted_message, derived_key):
         """Sends an encrypted message over a TCP socket"""
         if encrypted_message is None:
             raise ValueError("Encrypted message cannot be None")
-        obfuscated_message = Obfuscation.obfuscate_data(encrypted_message)
+        cca_tag = CCAProtection.protect_against_cca(derived_key, encrypted_message)
+        obfuscated_message = Obfuscation.obfuscate_data(encrypted_message + cca_tag)
         message_length = len(obfuscated_message)
         sock.sendall(struct.pack("!I", message_length))
         sock.sendall(obfuscated_message)
 
     @staticmethod
-    def receive_encrypted_message(sock):
+    def receive_encrypted_message(sock, derived_key):
         """Receives an encrypted message over a TCP socket"""
         message_length_data = sock.recv(4)
         if not message_length_data:
@@ -24,14 +24,20 @@ class TCPTransport:
         obfuscated_message = sock.recv(message_length)
         if not obfuscated_message:
             return None
-        return Obfuscation.deobfuscate_data(obfuscated_message)
+        decrypted_message_with_tag = Obfuscation.deobfuscate_data(obfuscated_message)
+        decrypted_message = decrypted_message_with_tag[:-32]  # remove CCA tag
+        expected_cca_tag = decrypted_message_with_tag[-32:]
+        if not CCAProtection.verify_cca_protection(derived_key, decrypted_message, expected_cca_tag):
+            raise ValueError("CCA protection verification failed")
+        return decrypted_message
 
     @staticmethod
-    def send_encrypted_file(sock, encrypted_file_data, file_name):
+    def send_encrypted_file(sock, encrypted_file_data, file_name, derived_key):
         """Sends an encrypted file over a TCP socket"""
         if encrypted_file_data is None:
             raise ValueError("Encrypted file data cannot be None")
-        obfuscated_file_data = Obfuscation.obfuscate_data(encrypted_file_data)
+        cca_tag = CCAProtection.protect_against_cca(derived_key, encrypted_file_data)
+        obfuscated_file_data = Obfuscation.obfuscate_data(encrypted_file_data + cca_tag)
         file_name_encoded = file_name.encode()
         file_name_length = len(file_name_encoded)
         file_data_length = len(obfuscated_file_data)
@@ -42,7 +48,7 @@ class TCPTransport:
         sock.sendall(obfuscated_file_data)
 
     @staticmethod
-    def receive_encrypted_file(sock):
+    def receive_encrypted_file(sock, derived_key):
         """Receives an encrypted file over a TCP socket"""
         file_name_length_data = sock.recv(4)
         if not file_name_length_data:
@@ -53,21 +59,29 @@ class TCPTransport:
         file_data_length_data = sock.recv(8)
         file_data_length = struct.unpack("!Q", file_data_length_data)[0]
         obfuscated_file_data = sock.recv(file_data_length)
-
-        return file_name, Obfuscation.deobfuscate_data(obfuscated_file_data)
+        if not obfuscated_file_data:
+            return None, None
+        
+        decrypted_file_data_with_tag = Obfuscation.deobfuscate_data(obfuscated_file_data)
+        decrypted_file_data = decrypted_file_data_with_tag[:-32]  # remove CCA tag
+        expected_cca_tag = decrypted_file_data_with_tag[-32:]
+        if not CCAProtection.verify_cca_protection(derived_key, decrypted_file_data, expected_cca_tag):
+            raise ValueError("CCA protection verification failed")
+        return file_name, decrypted_file_data
 
 class UDPTransport:
     @staticmethod
-    def send_encrypted_message(sock, encrypted_message, address):
+    def send_encrypted_message(sock, encrypted_message, address, derived_key):
         """Sends an encrypted message over a UDP socket"""
         if encrypted_message is None:
             raise ValueError("Encrypted message cannot be None")
-        obfuscated_message = Obfuscation.obfuscate_data(encrypted_message)
+        cca_tag = CCAProtection.protect_against_cca(derived_key, encrypted_message)
+        obfuscated_message = Obfuscation.obfuscate_data(encrypted_message + cca_tag)
         message_length = len(obfuscated_message)
         sock.sendto(struct.pack("!I", message_length) + obfuscated_message, address)
 
     @staticmethod
-    def receive_encrypted_message(sock):
+    def receive_encrypted_message(sock, derived_key):
         """Receives an encrypted message over a UDP socket"""
         message_length_data, _ = sock.recvfrom(4)
         if not message_length_data:
@@ -76,14 +90,20 @@ class UDPTransport:
         obfuscated_message, _ = sock.recvfrom(message_length)
         if not obfuscated_message:
             return None
-        return Obfuscation.deobfuscate_data(obfuscated_message)
+        decrypted_message_with_tag = Obfuscation.deobfuscate_data(obfuscated_message)
+        decrypted_message = decrypted_message_with_tag[:-32]  # remove CCA tag
+        expected_cca_tag = decrypted_message_with_tag[-32:]
+        if not CCAProtection.verify_cca_protection(derived_key, decrypted_message, expected_cca_tag):
+            raise ValueError("CCA protection verification failed")
+        return decrypted_message
 
     @staticmethod
-    def send_encrypted_file(sock, encrypted_file_data, file_name, address):
+    def send_encrypted_file(sock, encrypted_file_data, file_name, address, derived_key):
         """Sends an encrypted file over a UDP socket"""
         if encrypted_file_data is None:
             raise ValueError("Encrypted file data cannot be None")
-        obfuscated_file_data = Obfuscation.obfuscate_data(encrypted_file_data)
+        cca_tag = CCAProtection.protect_against_cca(derived_key, encrypted_file_data)
+        obfuscated_file_data = Obfuscation.obfuscate_data(encrypted_file_data + cca_tag)
         file_name_encoded = file_name.encode()
         file_name_length = len(file_name_encoded)
         file_data_length = len(obfuscated_file_data)
@@ -91,7 +111,7 @@ class UDPTransport:
         sock.sendto(struct.pack("!I", file_name_length) + file_name_encoded + struct.pack("!Q", file_data_length) + obfuscated_file_data, address)
 
     @staticmethod
-    def receive_encrypted_file(sock):
+    def receive_encrypted_file(sock, derived_key):
         """Receives an encrypted file over a UDP socket"""
         file_name_length_data, _ = sock.recvfrom(4)
         if not file_name_length_data:
@@ -102,44 +122,12 @@ class UDPTransport:
         file_data_length_data, _ = sock.recvfrom(8)
         file_data_length = struct.unpack("!Q", file_data_length_data)[0]
         obfuscated_file_data, _ = sock.recvfrom(file_data_length)
-
-        return file_name.decode(), Obfuscation.deobfuscate_data(obfuscated_file_data)
-
-class HTTPTransport:
-    @staticmethod
-    def send_encrypted_message(url, encrypted_message):
-        """Sends an encrypted message over HTTP"""
-        if encrypted_message is None:
-            raise ValueError("Encrypted message cannot be None")
-        obfuscated_message = Obfuscation.obfuscate_data(encrypted_message)
-        response = requests.post(url, data=obfuscated_message)
-        return response.status_code
-
-    @staticmethod
-    def receive_encrypted_message(url):
-        """Receives an encrypted message over HTTP"""
-        response = requests.get(url)
-        if response.status_code != 200:
-            return None
-        obfuscated_message = response.content
-        return Obfuscation.deobfuscate_data(obfuscated_message)
-
-    @staticmethod
-    def send_encrypted_file(url, encrypted_file_data, file_name):
-        """Sends an encrypted file over HTTP"""
-        if encrypted_file_data is None:
-            raise ValueError("Encrypted file data cannot be None")
-        obfuscated_file_data = Obfuscation.obfuscate_data(encrypted_file_data)
-        files = {'file': (file_name, obfuscated_file_data)}
-        response = requests.post(url, files=files)
-        return response.status_code
-
-    @staticmethod
-    def receive_encrypted_file(url):
-        """Receives an encrypted file over HTTP"""
-        response = requests.get(url)
-        if response.status_code != 200:
+        if not obfuscated_file_data:
             return None, None
-        file_name = response.headers.get('Content-Disposition').split('filename=')[1]
-        obfuscated_file_data = response.content
-        return file_name, Obfuscation.deobfuscate_data(obfuscated_file_data)
+        
+        decrypted_file_data_with_tag = Obfuscation.deobfuscate_data(obfuscated_file_data)
+        decrypted_file_data = decrypted_file_data_with_tag[:-32]  # remove CCA tag
+        expected_cca_tag = decrypted_file_data_with_tag[-32:]
+        if not CCAProtection.verify_cca_protection(derived_key, decrypted_file_data, expected_cca_tag):
+            raise ValueError("CCA protection verification failed")
+        return file_name.decode(), decrypted_file_data
