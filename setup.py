aecf8ab70b313b742dccb02ed5f012e0c6c6179f
certikpolik30 <evilpostma@gmail.com>
2025-03-20 10:19:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mvproto',
+    name='libdemon',
     version='0.6',
     packages=find_packages(),
     description='A secure communication protocol for e2ee.',
